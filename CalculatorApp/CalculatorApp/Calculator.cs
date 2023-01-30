15f1d0b70883e4e18b12fbef5570536549ec0d79
Mohammad Nur <mdnur701@gmail.com>
2023-01-30 20:26:56 +0600

    Week-2 task done

---
+++ CalculatorApp/CalculatorApp/Calculator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+namespace CalculatorApp
+{
+    public class Calculator
+    {
+        private int number1;
+        private int number2;
+
+        public Calculator()
+        {
+
+        }
+        public void setNumber1(int number1)
+        {
+            this.number1 = number1;
+        }
+        public int getNumber1()
+        {
+            return this.number1;
+        }
+
+        public void setNumber2(int number2)
+        {
+            this.number2 = number2;
+        }
+        public int getNumber2()
+        {
+            return this.number2;
+        }
+
+        //public int sum(int number1, int number2)
+        public int sum()
+        {
+            return this.number1 + number2;
+        }
+
+        //public int sub(int number1, int number2)
+             public int sub()
+        {
+            return this.number1 - number2;
+        }
+
+        //public int mal(int number1, int number2)
+            public int mal()
+        {
+            return this.number1 * number2;
+        }
+        //public int div(int number1, int number2)
+        public int div()
+        {
+            return this.number1 / number2;
+        }
+        public int sqaure()
+        {
+            return (int)Math.Pow(number1,number2);
+        }
+        public double remainder()
+        {
+            return number1 % number2;
+        }
+
+        public double percent(int number1)
+        {
+            return number1 / 100;
+        }
+        public int max()
+        {
+            return Math.Max(this.number1, this.number2);
+        }
+        public int min()
+        {
+            return Math.Min(this.number1, this.number2);
+        }
+    }
+}
---
+++ CalculatorApp/CalculatorApp/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace CalculatorApp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter Number 1: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Number 2: ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine();
+            Console.WriteLine("Which Operation do you want to ");
+            Console.WriteLine("1.Sum");
+            Console.WriteLine("2.Sub");
+            Console.WriteLine("3.mal");
+            Console.WriteLine("4.div");
+            Console.WriteLine("5.Remainder");
+            Console.WriteLine("6.To The power");
+            Console.WriteLine("7.Percentage");
+            Console.WriteLine("8.Min");
+            Console.WriteLine("9.Max");
+            Console.WriteLine("10.Exit");
+            int choose = Convert.ToInt32(Console.ReadLine());
+            Calculator cal = new Calculator();
+            cal.setNumber1(number1);
+            cal.setNumber2(number2);
+
+            while (true)
+            {
+                switch (choose)
+                {
+                    case 1:
+                        Console.WriteLine(cal.sum());
+                        break;
+                    case 2:
+                        Console.WriteLine(cal.sub());
+                        break;
+                    case 3:
+                        Console.WriteLine(cal.mal());
+                        break;
+                    case 4:
+                        Console.WriteLine(cal.div());
+                        break;
+                    case 5:
+                        Console.WriteLine(cal.remainder());
+                        break;
+                    case 6:
+                        Console.WriteLine(cal.sqaure());
+                        break;
+                    case 7:
+                        Console.WriteLine(cal.percent(number1));
+                        break;
+                    case 8:
+                        Console.WriteLine(cal.min());
+                        break;
+                    case 9:
+                        Console.WriteLine(cal.max());
+                        break;
+                    case 10:
+                            break;
+                        break;
+                    default:
+                        Console.WriteLine("Wrong Input");
+                        break;
+                }
+
+            }
+            
+
+
+        }
+
+
+    }
+}
